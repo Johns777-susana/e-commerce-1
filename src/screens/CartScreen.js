a04cbf416c731c52ef587a67f45c24e32c468654
Johns777-susana <senma777@gmail.com>
2021-11-09 22:23:54 +0545

    cartscreen complete

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Row, Col, Image, Form, Button, ListGroup } from 'react-bootstrap';
+import { Row, Col, Image, Form, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { FaTimes } from 'react-icons/fa';
 import NavbarSecondary from '../components/navbar/NavbarSecondary';
 import staticData from '../data/staticData';
+import Benefits from '../components/Benefits';
+import Footer from '../components/Footer';
 
 const CartScreen = () => {
   return (
@@ -11,7 +13,7 @@ const CartScreen = () => {
       <NavbarSecondary />
 
       <Row className='cart-screen-container'>
-        <Col md={8}>
+        <Col md={10}>
           <Col className='flex-row'>
             <Link to='/' className='home-link'>
               Home
@@ -22,40 +24,137 @@ const CartScreen = () => {
             <h1>Shopping Cart</h1>
           </Col>
           <Col>
-            <ListGroup variant='flush'>
-              {staticData.map((item) => (
-                <ListGroup key={item._id}>
+            <Container fluid>
+              <Row className='cart-items-container'>
+                <Col xs={8} className='cart-first-container'>
+                  <Row className='cart-headers'>
+                    <Col></Col>
+                    <Col xs={4}>Item</Col>
+                    <Col>Price</Col>
+                    <Col>Quantity</Col>
+                    <Col>Total</Col>
+                    <Col></Col>
+                  </Row>
+                  {staticData.map((item) => (
+                    <Row className='cart-items-details'>
+                      <Col xs={2}>
+                        <Image
+                          src={item.image}
+                          alt={item.name}
+                          className='cart-product-image'
+                        />
+                      </Col>
+                      <Col xs={4}>
+                        <span style={{ fontWeight: 'bold' }}>
+                          <p style={{ marginBottom: '0rem' }}>{item.name}</p>
+                        </span>
+                        <p style={{ color: '#868e96' }}>
+                          Category: {item.category}
+                        </p>
+                      </Col>
+                      <Col style={{ paddingTop: '1rem' }}>
+                        <p>${item.price}</p>
+                      </Col>
+                      <Col>
+                        <Form.Control as='select' className='quantity-form'>
+                          {[...Array(item.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Col>
+                      <Col>
+                        <p
+                          style={{ paddingLeft: '0.5rem', paddingTop: '1rem' }}
+                        >
+                          $123
+                        </p>
+                      </Col>
+                      <Col>
+                        <button className='cross-btn-cart'>
+                          <FaTimes />
+                        </button>
+                      </Col>
+                    </Row>
+                  ))}
+                </Col>
+                <Col className='order-container'>
+                  <Row className='order-header'>
+                    <Col>order summary</Col>
+                  </Row>
                   <Row>
-                    <Col md={2}>
-                      <Image src={item.image} alt={item.name} fluid rounded />
+                    <p
+                      style={{
+                        fontSize: '0.7875rem',
+                      }}
+                    >
+                      Shipping and additional costs are calculated based on
+                      values you have entered.
+                    </p>
+                  </Row>
+                  <Row
+                    style={{
+                      borderBottom: '1px solid #eee',
+                      marginBottom: '1rem',
+                      marginTop: '1rem',
+                    }}
+                  >
+                    <Col xs={8}>
+                      <p>Order Subtotal</p>
                     </Col>
-                    <Col md={3}>
-                      <Link to='/product' className='home-link'>
-                        {item.name}
-                      </Link>
+                    <Col>$390.00</Col>
+                  </Row>
+                  <Row
+                    style={{
+                      borderBottom: '1px solid #eee',
+                      marginBottom: '1rem',
+                    }}
+                  >
+                    <Col xs={8}>
+                      <p>Shipping and handling</p>
                     </Col>
-                    <Col md={2}>${item.price}</Col>
-                    <Col md={2}>
-                      <Form.Control as='select'>
-                        {[...Array(item.countInStock).keys()].map((x) => (
-                          <option value={x + 1} key={x + 1}>
-                            {x + 1}
-                          </option>
-                        ))}
-                      </Form.Control>
+                    <Col>$10.00</Col>
+                  </Row>
+                  <Row
+                    style={{
+                      borderBottom: '1px solid #eee',
+                      marginBottom: '1rem',
+                    }}
+                  >
+                    <Col xs={8}>
+                      <p>Tax</p>
                     </Col>
-                    <Col md={2}>
-                      <button>
-                        <FaTimes />
-                      </button>
+                    <Col>$10.00</Col>
+                  </Row>
+                  <Row
+                    style={{
+                      marginBottom: '0.5rem',
+                    }}
+                  >
+                    <Col xs={8}>
+                      <p>Total</p>
+                    </Col>
+                    <Col>
+                      <p style={{ fontWeight: 'bold' }}>$400.00</p>
                     </Col>
                   </Row>
-                </ListGroup>
-              ))}
-            </ListGroup>
+                </Col>
+              </Row>
+            </Container>
           </Col>
         </Col>
       </Row>
+      <div className='cart-btns'>
+        <Link to='/products' className='home-link'>
+          <button className='continue-shopping-btn'>
+            &#8592; Continue Shopping
+          </button>
+        </Link>
+        <button className='checkout-btn'>Proceed to checkout &#8594;</button>
+      </div>
+      <Benefits />
+      <Footer />
     </>
   );
 };
